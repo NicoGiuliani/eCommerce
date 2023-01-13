e404b68137b52b58be39315a03de27508ade64c9
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-01-12 19:13:41 -0800

    Added the rest of the models

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,20 +1,24 @@
 from django.contrib import admin
-from .models import Product
+from .models import *
 
 # Register your models here.
-class ProductAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "brand",
-        "category",
-        "description",
-        "rating",
-        "numReviews",
-        "price",
-        "countInStock",
-        "createdAt",
-        "_id",
-    )
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "name",
+#         "brand",
+#         "category",
+#         "description",
+#         "rating",
+#         "numReviews",
+#         "price",
+#         "countInStock",
+#         "createdAt",
+#         "_id",
+#     )
 
 
-admin.site.register(Product, ProductAdmin)
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
