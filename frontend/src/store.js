82d333e7003b00870c3a0d32399e5301b3f5a26f
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-01-14 22:53:18 -0800

    Redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,17 +1,21 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { productListReducer } from "./reducers/productReducers";
+import { configureStore } from "@reduxjs/toolkit";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = configureStore({
+  reducer: reducer,
+  middleware: middleware,
+  preloadedState: initialState,
+});
 
 export default store;
